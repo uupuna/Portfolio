54a5344e0b633f34d4e193fab76a32decc45939a
Qianru Zhang <111253510+uupuna@users.noreply.github.com>
2023-12-18 12:00:02 -0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
+    # os.path.join(BASE_DIR, 'media')
 ]
 
 STATIC_URL = "/static/"
